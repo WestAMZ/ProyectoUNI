40740614bfd0c4ba6ebdb8a4301d697d37d0e810
MSCorpProgramador02 <programador02@mscorp.group>
2019-02-12 13:32:43 -0600

    Probando conexion

--- PAEDUCA/PAEDUCA/DAL/Conexion.cs
+++ PAEDUCA/PAEDUCA/DAL/Conexion.cs
@@ -4,24 +4,26 @@ using System.Data.Entity.Core.EntityClient;
 using System.Linq;
 using System.Web;
 using PAEDUCA.Controllers.AppConfigs;
+using System.Data.Entity.Infrastructure;
 
 namespace PAEDUCA.DAL
 {
     public class Conexion
     {
 
-        public static EntityConnectionStringBuilder conexion = null;
-        public static String obtenerConexion() 
+        public string conexion = null;
+        private EntityConnectionStringBuilder builder = null;
+        public  Conexion()
         { 
-            if(conexion==null)
+            if(builder==null)
             {
-                conexion = new EntityConnectionStringBuilder();
-                conexion.Provider = "System.Data.SqlClient";
-                conexion.ProviderConnectionString = "Data Source="+Configuraciones.USUARIO_BD+";Initial Catalog=+"+Configuraciones.NOMBRE_BD+";User ID="+Configuraciones.USUARIO_BD+";Password="+Configuraciones.PASS_BD+";multipleactiveresultsets=True;application name=EntityFramework";
-                conexion.Metadata = "res://*/EF_PAEDUCA.csdl|res://*/EF_PAEDUCA.ssdl|res://*/EF_PAEDUCA.msl";
+                builder = new EntityConnectionStringBuilder();
+                builder.Provider = "System.Data.SqlClient";
+                builder.ProviderConnectionString = "Data Source="+Configuraciones.USUARIO_BD+";Initial Catalog=+"+Configuraciones.NOMBRE_BD+";User ID="+Configuraciones.USUARIO_BD+";Password="+Configuraciones.PASS_BD+";multipleactiveresultsets=True;application name=EntityFramework";
+                //builder.Metadata = "res://*/EF_PAEDUCA.csdl|res://*/EF_PAEDUCA.ssdl|res://*/EF_PAEDUCA.msl";
             }
 
-            return conexion.ToString();
+            conexion =  builder.ToString();
         }
 
     }
--- PAEDUCA/PAEDUCA/DAL/PAEDUCAContext.cs
+++ PAEDUCA/PAEDUCA/DAL/PAEDUCAContext.cs
@@ -8,7 +8,7 @@
     {
    
         public PAEDUCAContext()
-            : base("Name=MS")
+            : base(new Conexion().conexion)
         {
 
         }
